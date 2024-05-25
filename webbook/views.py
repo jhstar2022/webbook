03a78f98f0c2e112dad02634b9f86d564b07e4e0
Jounghyo Lee <tlfqj0617@naver.com>
2024-05-25 20:33:45 +0900

    2024.05.25

--- webbook/views.py
+++ webbook/views.py
@@ -1,7 +1,11 @@
-from django.views import View
 from django.shortcuts import render
+from django.views import View
 
 
 class index(View):
     def get(self,request):
         return render(request, 'webbook/index.html')
+
+class base(View):
+    def get(self,request):
+        return render(request, 'webbook/base.html')
