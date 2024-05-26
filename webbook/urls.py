5119dfac78f8b03e49aab9f524b640c78b012988
Jounghyo Lee <tlfqj0617@naver.com>
2024-05-26 23:21:25 +0900

    2024.05.26

--- webbook/urls.py
+++ webbook/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index.as_view()),
     path('base/', views.base.as_view()),
--- webbook/views.py
+++ webbook/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from django.views import View
+from django.shortcuts import render
 
 
 class index(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, 'webbook/index.html')
 
+
 class base(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, 'webbook/base.html')
