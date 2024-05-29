14dc58c7853f78a0ea00f3b413fd10379695d58d
Jounghyo Lee <tlfqj0617@naver.com>
2024-05-29 23:40:15 +0900

    2024.05.29

--- webbook/urls.py
+++ webbook/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index.as_view()),
+    path('', views.Index.as_view(), name='lise'),
     path('base/', views.base.as_view()),
 ]
--- webbook/views.py
+++ webbook/views.py
@@ -1,11 +1,16 @@
 from django.views import View
 from django.shortcuts import render
+from .models import Post
 
 
-class index(View):
+class Index(View):
     def get(self, request):
-        return render(request, 'webbook/index.html')
-
+        posts = Post.objects.all()
+        context = {
+            'posts': posts,
+            'title': 'blog'
+        }
+        return render(request, 'webbook/index.html', context)
 
 class base(View):
     def get(self, request):
