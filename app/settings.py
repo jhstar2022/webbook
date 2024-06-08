b33bd7b45d5953996cf9f4494fb16fa745e6ff74
Jounghyo Lee <tlfqj0617@naver.com>
2024-06-08 20:30:01 +0900

    2024.06.08

--- app/settings.py
+++ app/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+AUTH_USER_MODEL = 'user.User'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+LOGIN_URL = '/user/login'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webbook',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- webbook/models.py
+++ webbook/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
-
+User = get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
