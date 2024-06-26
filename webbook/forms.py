f4d348f993f8ef88079412077949b3d12d6b1df8
Jounghyo Lee <tlfqj0617@naver.com>
2024-06-27 02:56:04 +0900

    2024.06.27

--- webbook/forms.py
+++ webbook/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Novel, Chapter
 
 
 class PostForm(forms.ModelForm):
@@ -7,3 +7,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+
+class NovelForm(forms.ModelForm):
+    class Meta:
+        model = Novel
+        fields = ['title', 'genre', 'description']
+
+
+class ChapterForm(forms.ModelForm):
+    class Meta:
+        model = Chapter
+        fields = ['number', 'title', 'content']
--- webbook/views.py
+++ webbook/views.py
@@ -1,10 +1,11 @@
 from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post
+from .models import Post, Novel, Chapter, UserNovelPermission
 from .forms import PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.decorators import login_required
+from .forms import NovelForm, ChapterForm
 
 
 class Index(View):
@@ -97,3 +98,88 @@ class DetailView(View):
         return render(request, 'webbook/post_detail.html', context)
 
 
+@login_required
+def novel_list(request):
+    novels = Novel.objects.filter(user_permissions__user=request.user, user_permissions__can_read=True)
+    return render(request, 'novels/novel_list.html', {'novels': novels})
+
+@login_required
+def novel_detail(request, novel_id):
+    novel = get_object_or_404(Novel, id=novel_id)
+    permission = get_object_or_404(UserNovelPermission, user=request.user, novel=novel, can_read=True)
+    return render(request, 'novels/novel_detail.html', {'novel': novel})
+
+@login_required
+def chapter_detail(request, novel_id, chapter_number):
+    novel = get_object_or_404(Novel, id=novel_id)
+    permission = get_object_or_404(UserNovelPermission, user=request.user, novel=novel, can_read=True)
+    chapter = get_object_or_404(Chapter, novel=novel, number=chapter_number)
+    return render(request, 'chapters/chapter_detail.html', {'chapter': chapter})
+
+@login_required
+def create_novel(request):
+    if request.method == 'POST':
+        form = NovelForm(request.POST)
+        if form.is_valid():
+            novel = form.save(commit=False)
+            novel.author = request.user
+            novel.save()
+            UserNovelPermission.objects.create(user=request.user, novel=novel, can_read=True)
+            return redirect('novel_detail', novel_id=novel.id)
+    else:
+        form = NovelForm()
+    return render(request, 'novels/novel_form.html', {'form': form})
+
+@login_required
+def update_novel(request, novel_id):
+    novel = get_object_or_404(Novel, id=novel_id, author=request.user)
+    if request.method == 'POST':
+        form = NovelForm(request.POST, instance=novel)
+        if form.is_valid():
+            form.save()
+            return redirect('novel_detail', novel_id=novel.id)
+    else:
+        form = NovelForm(instance=novel)
+    return render(request, 'novels/novel_form.html', {'form': form})
+
+@login_required
+def delete_novel(request, novel_id):
+    novel = get_object_or_404(Novel, id=novel_id, author=request.user)
+    if request.method == 'POST':
+        novel.delete()
+        return redirect('novel_list')
+    return render(request, 'novels/novel_confirm_delete.html', {'novel': novel})
+
+@login_required
+def create_chapter(request, novel_id):
+    novel = get_object_or_404(Novel, id=novel_id, author=request.user)
+    if request.method == 'POST':
+        form = ChapterForm(request.POST)
+        if form.is_valid():
+            chapter = form.save(commit=False)
+            chapter.novel = novel
+            chapter.save()
+            return redirect('chapter_detail', novel_id=novel.id, chapter_number=chapter.number)
+    else:
+        form = ChapterForm()
+    return render(request, 'chapters/chapter_form.html', {'form': form})
+
+@login_required
+def update_chapter(request, novel_id, chapter_number):
+    chapter = get_object_or_404(Chapter, novel_id=novel_id, number=chapter_number, novel__author=request.user)
+    if request.method == 'POST':
+        form = ChapterForm(request.POST, instance=chapter)
+        if form.is_valid():
+            form.save()
+            return redirect('chapter_detail', novel_id=novel_id, chapter_number=chapter.number)
+    else:
+        form = ChapterForm(instance=chapter)
+    return render(request, 'chapters/chapter_form.html', {'form': form})
+
+@login_required
+def delete_chapter(request, novel_id, chapter_number):
+    chapter = get_object_or_404(Chapter, novel_id=novel_id, number=chapter_number, novel__author=request.user)
+    if request.method == 'POST':
+        chapter.delete()
+        return redirect('novel_detail', novel_id=novel_id)
+    return render(request, 'chapters/chapter_confirm_delete.html', {'chapter': chapter})
